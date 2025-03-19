a7e7fd6df3e009ec9c98f78a8051bb38f754ef96
goldiefever <mgoldhaberfiebert2028@kehillahstudent.org>
2025-03-19 12:29:33 -0700

    done!

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
+import string
+
 text = """
 Everyone counts.
 Everyone has access to great learning.
 Every person's unique talents are valued equally.
-Everyone takes responsibility for themselves and their community."""
+Everyone takes responsibility for themselves and their community.
+"""
 
 # Make a dictionary where the keys are characters and the values are the number of times that character appears in the `text` print the dicitonary
+
+character_values = {}
+
+for letter in text:
+    if not letter in string.ascii_letters:
+        pass
+    elif letter.lower() in character_values:
+        character_values[letter.lower()] += 1
+    else:
+        character_values[letter.lower()] = 1
+
+print(character_values)
